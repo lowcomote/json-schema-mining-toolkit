cd0930eee030db6e45241c7120bfbcf7b2373f60
alessandro.colantoni <alessandro.colantoni@jku.at>
2021-01-17 21:11:09 +0100

    Test draft 3. Used processingMessage.asJson()

--- src/main/java/jku/bise/jsonschemavalidator/applicationservice/draftvalidator/Draft3SchemaValidator.java
+++ src/main/java/jku/bise/jsonschemavalidator/applicationservice/draftvalidator/Draft3SchemaValidator.java
@@ -24,14 +24,11 @@ public class Draft3SchemaValidator {
 	private static Logger logger = LoggerFactory.getLogger(Draft3SchemaValidator.class);
 	private  SyntaxValidator syntaxValidator;
 	
-	
-	
 	public Draft3SchemaValidator () {
 		ValidationConfigurationBuilder validationConfigurationBuilder=ValidationConfiguration.newBuilder();
 		validationConfigurationBuilder.setDefaultVersion(SchemaVersion.DRAFTV3);
 		ValidationConfiguration validationConfiguration = validationConfigurationBuilder.freeze();
 		this.syntaxValidator = new SyntaxValidator(validationConfiguration);
-		
 	}
 	
 	public List<String> validate (File file) throws SchemaValidatorException {
@@ -47,8 +44,21 @@ public class Draft3SchemaValidator {
 		List<String> messages = new ArrayList<String>();
 		ProcessingReport processingReport =  this.syntaxValidator.validateSchema(schema);
 		processingReport.forEach(processingMessage->{
-			String message = processingMessage.getMessage();
+			JsonNode processingMessageJson = processingMessage.asJson();
+			String message = processingMessageJson.toPrettyString();
 			messages.add(message);
+			if(logger.isDebugEnabled()) {
+				logger.debug("Json-schema-validator pretty string: {}",message);
+				
+			}
+			
+			
+//			String message = processingMessage.getMessage(); //processingMessage.
+//			messages.add(message);
+//			if(logger.isDebugEnabled()) {
+//				logger.debug("Json-schema-validator message: {}",message);
+//				
+//			}
 		});
 		return messages;
 	}
